e33e57247bce3ce96b840edaecdb6cdb2914066b
guoby <guobingyi@gmail.com>
2016-02-05 16:18:33 +0800

    test add java code

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("2&1:"+(2&1));
+		System.out.println("0&1:"+(0&1));
+		System.out.println("1&1:"+(1&1));
+		System.out.println("2|1:"+(2|1));
+		System.out.println("0|1:"+(0|1));
+		System.out.println("0|0:"+(0|1));
+
+	}
+
+}
